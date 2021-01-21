497f29737c0634e9a23714029cd9029658cd7637
Robear Selwans <robear.selwans@outlook.com>
2021-01-21 17:00:27 +0200

    Minor Changes
    Signed-off-by: Robear Selwans <robear.selwans@outlook.com>

--- src/mod.c
+++ src/mod.c
@@ -1,5 +1,6 @@
 #define EV_MODULE_DEFINE
 #include <evol/evolmod.h>
+
 #include <GLFW/glfw3.h>
 
 #include <assert.h>
@@ -14,8 +15,8 @@
 #include <string.h>
 
 struct ev_WindowData {
-    unsigned int width;
-    unsigned int height;
+    U32 width;
+    U32 height;
 
     char windowTitle[MAX_WINDOW_TITLE_LENGTH];
     GLFWwindow *windowHandle;
@@ -23,14 +24,26 @@ struct ev_WindowData {
     bool created;
 } WindowData;
 
-EV_CONSTRUCTOR {
-  glfwInit();
+struct ev_InputData {
+  U32 dummy;
+};
+
+EV_CONSTRUCTOR
+{
+  if(!glfwInit()) {
+    goto glfw_initialization_failed;
+  }
 
   WindowData.width = DEFAULT_WINDOW_WIDTH;
   WindowData.height = DEFAULT_WINDOW_HEIGHT;
   strcpy(WindowData.windowTitle, DEFAULT_WINDOW_TITLE);
   WindowData.windowHandle = NULL;
   WindowData.created = false;
+
+  return 0;
+
+glfw_initialization_failed:
+  return 1;
 }
 
 EV_EXPORT bool createWindow()
@@ -49,17 +62,27 @@ EV_EXPORT void windowLoop()
 }
 
 EV_START {
-  if(createWindow()) {
-    WindowData.created = true;
-    windowLoop();
+  if(!createWindow()) {
+    goto window_creation_failed;
   }
+  WindowData.created = true;
+
+  windowLoop();
+
+  return 0;
+
+window_creation_failed:
+    return 1;
 }
 
-EV_DESTRUCTOR {
-  if(WindowData.windowHandle) {
+EV_DESTRUCTOR
+{
+  if(WindowData.created) {
     glfwDestroyWindow(WindowData.windowHandle);
   }
   glfwTerminate();
+
+  return 0; // Don't know of a way for these 2 functions to fail
 }
 
 void _updateWindowSize()
@@ -67,19 +90,19 @@ void _updateWindowSize()
   glfwSetWindowSize(WindowData.windowHandle, WindowData.width, WindowData.height);
 }
 
-EV_EXPORT void setWindowHeight(uint32_t height)
+EV_EXPORT void setWindowHeight(U32 height)
 {
   WindowData.height = height;
   _updateWindowSize();
 }
 
-EV_EXPORT void setWindowWidth(uint32_t width)
+EV_EXPORT void setWindowWidth(U32 width)
 {
   WindowData.width = width;
   _updateWindowSize();
 }
 
-EV_EXPORT void setWindowSize(uint32_t width, uint32_t height)
+EV_EXPORT void setWindowSize(U32 width, U32 height)
 {
   WindowData.width = width;
   WindowData.height = height;
